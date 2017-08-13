e3512619f38f386492438a3815214910e66c6ae1
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2017-08-13 15:29:44 +0530

    routes

--- app/src/main/java/com/example/pulkit/locationdemo2/MapsActivity.java
+++ app/src/main/java/com/example/pulkit/locationdemo2/MapsActivity.java
@@ -2,11 +2,16 @@ package com.example.pulkit.locationdemo2;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.pulkit.locationdemo2.MODEL.DirectionResults;
+import com.example.pulkit.locationdemo2.MODEL.Location;
+import com.example.pulkit.locationdemo2.MODEL.Route;
+import com.example.pulkit.locationdemo2.MODEL.Steps;
 import com.example.pulkit.locationdemo2.REST.ApiClients;
 import com.example.pulkit.locationdemo2.REST.ApiInterface;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -18,6 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,15 +36,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     FusedLocationProviderClient mFusedLocationProviderClient;
-
-    LocationRequest mLocationRequest;
-
     LocationCallback mLocationCallback;
-
     boolean mRequestingLocationUpdates;
-
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-
+    List<LatLng> points= new ArrayList<>();
+    MarkerOptions markerOptions = new MarkerOptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +50,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        points.add(new LatLng(28.5244,77.1855));
+        points.get(0);
 
     }
 
@@ -60,11 +67,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         fetchroutes();
-
-////        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-////        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-////        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     private void fetchroutes() {
@@ -73,6 +75,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         call.enqueue(new Callback<DirectionResults>() {
             @Override
             public void onResponse(Call<DirectionResults> call, Response<DirectionResults> response) {
+                ArrayList<LatLng> routelist = new ArrayList<LatLng>();
+                if(response.body().getRoutes().size()>0){
+                    ArrayList<LatLng> decodelist;
+                    Route routeA = response.body().getRoutes().get(0);
+                    if(routeA.getLegs().size()>0){
+                        List<Steps> steps= routeA.getLegs().get(0).getSteps();
+                        Steps step;
+                        Location location;
+                        String polyline;
+                        for(int i=0 ; i<steps.size();i++){
+                            step = steps.get(i);
+                            location =step.getStart_location();
+                            routelist.add(new LatLng(location.getLat(), location.getLng()));
+                            polyline = step.getPolyline().getPoints();
+                            decodelist = RouteDecode.decodePoly(polyline);
+                            routelist.addAll(decodelist);
+                            location =step.getEnd_location();
+                            routelist.add(new LatLng(location.getLat() ,location.getLng()));
+                        }
+                    }
+                }
+                if(routelist.size()>0){
+                    PolylineOptions rectLine = new PolylineOptions().width(10).color(
+                            Color.BLUE);
+
+                    for (int i = 0; i < routelist.size(); i++) {
+                        rectLine.add(routelist.get(i));
+                    }
+                    // Adding route on the map
+                    mMap.addPolyline(rectLine);
+                    markerOptions.position(points.get(0));
+                    markerOptions.draggable(true);
+                    mMap.addMarker(markerOptions);
+                }
 
             }
 
