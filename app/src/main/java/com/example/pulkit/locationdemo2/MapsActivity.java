f09e50c7a4b04e40f671105848b680ab81a94594
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2017-08-14 09:10:23 +0530

    4 Locations

--- app/src/main/java/com/example/pulkit/locationdemo2/MapsActivity.java
+++ app/src/main/java/com/example/pulkit/locationdemo2/MapsActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.pulkit.locationdemo2.MODEL.DirectionResults;
 import com.example.pulkit.locationdemo2.MODEL.Location;
@@ -14,12 +15,13 @@ import com.example.pulkit.locationdemo2.MODEL.Route;
 import com.example.pulkit.locationdemo2.MODEL.Steps;
 import com.example.pulkit.locationdemo2.REST.ApiClients;
 import com.example.pulkit.locationdemo2.REST.ApiInterface;
+import com.flipboard.bottomsheet.BottomSheetLayout;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -51,7 +53,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     android.location.Location location;
     List<LatLng> points = new ArrayList<>();
     MarkerOptions markerOptions = new MarkerOptions();
-    private CameraPosition mCameraPosition;
+    private FloatingActionMenu menuRed;
+    private com.github.clans.fab.FloatingActionButton fab1;
+    private com.github.clans.fab.FloatingActionButton fab2;
+    private com.github.clans.fab.FloatingActionButton fab3;
+    private com.github.clans.fab.FloatingActionButton fab4;
+    private List<FloatingActionMenu> menus = new ArrayList<>();
+    BottomSheetLayout bottomSheet;
+    Polyline polylineFinal,polyline1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,17 +68,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         if (savedInstanceState != null) {
             location = savedInstanceState.getParcelable(KEY_LOCATION);
-            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+            CameraPosition mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
         }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        points.add(new LatLng(28.6127, 77.2773));
-        points.add(new LatLng(28.6129, 77.2295));
-        points.add(new LatLng(28.6881, 77.2069));
-        points.add(new LatLng(28.5244, 77.1855));
-//        getDeviceLocation();
+        points.add(new LatLng(28.6127, 77.2773));//Akshardham
+        points.add(new LatLng(28.6129, 77.2295));//India Gate
+        points.add(new LatLng(28.6881, 77.2069));//Faculty Of Maths
+        points.add(new LatLng(28.5244, 77.1855));//Qutub Minar
+        bottomSheet = findViewById(R.id.bottomsheet);
+        menuRed = findViewById(R.id.fab);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+        fab3 = findViewById(R.id.fab3);
+        fab4 = findViewById(R.id.fab4);
+        menuRed.setClosedOnTouchOutside(true);
+        menus.add(menuRed);
+        menuRed.setOnMenuButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuRed.toggle(true);
+            }
+        });
+        fab1.setOnClickListener(clickListener);
+        fab2.setOnClickListener(clickListener);
+        fab3.setOnClickListener(clickListener);
+        fab4.setOnClickListener(clickListener);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000)
+                .setFastestInterval(5000)
+                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+            }
+
+        };
     }
 
     @Override
@@ -90,29 +129,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    private void getDeviceLocation() {
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    private void getDeviceLocation(final LatLng latLng) {
 
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000)
-                .setFastestInterval(5000)
-                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        Log.i("Functionworking", "onClick: " + latLng);
 
         mLocationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 location = locationResult.getLastLocation();
-                points.set(0, new LatLng(location.getLatitude(), location.getLongitude()));
-                fetchroutes(location);
+                fetchroutes(location, latLng);
+
             }
 
         };
     }
 
-    private void fetchroutes(final android.location.Location lctn) {
+    private void fetchroutes(final android.location.Location lctn, final LatLng latLng) {
+        Log.i("Functionworking", "onClick: " + lctn + latLng);
         ApiInterface apiService = ApiClients.getClient().create(ApiInterface.class);
-        Call<DirectionResults> call = apiService.getRoutes(lctn.getLatitude() + "," + lctn.getLongitude(), "28.6881, 77.2069");
+        Call<DirectionResults> call = apiService.getRoutes(lctn.getLatitude() + "," + lctn.getLongitude(), latLng.latitude + "," + latLng.longitude);
         call.enqueue(new Callback<DirectionResults>() {
             @Override
             public void onResponse(Call<DirectionResults> call, Response<DirectionResults> response) {
@@ -145,11 +181,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         rectLine.add(routelist.get(i));
                     }
                     // Adding route on the map
-                    mMap.clear();
-                    mMap.addPolyline(rectLine);
-                    markerOptions.position(points.get(2));
+//                    mMap.clear();
+                    polylineFinal = mMap.addPolyline(rectLine);
+                    markerOptions.position(latLng);
                     markerOptions.draggable(true);
-                    markerOptions.title("Euclidean Distance " + computeDistanceBetween(new LatLng(lctn.getLatitude(), lctn.getLongitude()), points.get(2)) + "m");
+                    markerOptions.title("Euclidean Distance " + computeDistanceBetween(new LatLng(lctn.getLatitude(), lctn.getLongitude()), latLng) + "m");
                     mMap.addMarker(markerOptions);
                 }
 
@@ -160,10 +196,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         });
-        Polyline polyline1 = mMap.addPolyline(new PolylineOptions()
+        polyline1 = mMap.addPolyline(new PolylineOptions()
                 .add(
                         new LatLng(lctn.getLatitude(), lctn.getLongitude()),
-                        points.get(2)));
+                        latLng));
         polyline1.setColor(Color.RED);
     }
 
@@ -184,4 +220,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             super.onSaveInstanceState(outState);
         }
     }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.fab1:
+                    getDeviceLocation(points.get(0));
+                    mFusedLocationProviderClient
+                            .requestLocationUpdates(mLocationRequest,
+                                    mLocationCallback, null);
+                    mRequestingLocationUpdates = true;
+                    break;
+                case R.id.fab2:
+                    getDeviceLocation(points.get(1));
+                    mFusedLocationProviderClient
+                            .requestLocationUpdates(mLocationRequest,
+                                    mLocationCallback, null);
+                    mRequestingLocationUpdates = true;
+
+                    break;
+                case R.id.fab3:
+                    getDeviceLocation(points.get(2));
+                    mFusedLocationProviderClient
+                            .requestLocationUpdates(mLocationRequest,
+                                    mLocationCallback, null);
+                    mRequestingLocationUpdates = true;
+                    break;
+                case R.id.fab4:
+                    getDeviceLocation(points.get(3));
+                    mFusedLocationProviderClient
+                            .requestLocationUpdates(mLocationRequest,
+                                    mLocationCallback, null);
+                    mRequestingLocationUpdates = true;
+                    break;
+            }
+        }
+    };
+
 }
