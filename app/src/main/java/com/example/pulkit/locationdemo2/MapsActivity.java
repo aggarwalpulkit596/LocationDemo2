4180ceb506588ba9928b08302f5262006549e69e
aggarwalpulkit596 <aggarwalpulkit596@gmail.com>
2017-08-16 00:18:12 +0530

    Final

--- app/src/main/java/com/example/pulkit/locationdemo2/MapsActivity.java
+++ app/src/main/java/com/example/pulkit/locationdemo2/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.pulkit.locationdemo2;
 
 import android.Manifest;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.support.v4.app.ActivityCompat;
@@ -19,15 +20,21 @@ import com.example.pulkit.locationdemo2.REST.ApiClients;
 import com.example.pulkit.locationdemo2.REST.ApiInterface;
 import com.flipboard.bottomsheet.BottomSheetLayout;
 import com.github.clans.fab.FloatingActionMenu;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
@@ -50,18 +57,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     LocationRequest mLocationRequest;
     boolean mRequestingLocationUpdates;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    android.location.Location location;
+    android.location.Location location, previousLocation = null;
     List<LatLng> points = new ArrayList<>();
     MarkerOptions markerOptions = new MarkerOptions();
     private FloatingActionMenu menuRed;
-    private com.github.clans.fab.FloatingActionButton fab1;
-    private com.github.clans.fab.FloatingActionButton fab2;
-    private com.github.clans.fab.FloatingActionButton fab3;
-    private com.github.clans.fab.FloatingActionButton fab4;
     private List<FloatingActionMenu> menus = new ArrayList<>();
     BottomSheetLayout bottomSheet;
-    Polyline polylineFinal,polyline1;
-    String time,distance;
+    Polyline polylineFinal, polyline1;
+    String time, distance;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,10 +86,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         points.add(new LatLng(28.5244, 77.1855));//Qutub Minar
         bottomSheet = findViewById(R.id.bottomsheet);
         menuRed = findViewById(R.id.fab);
-        fab1 = findViewById(R.id.fab1);
-        fab2 = findViewById(R.id.fab2);
-        fab3 = findViewById(R.id.fab3);
-        fab4 = findViewById(R.id.fab4);
+        com.github.clans.fab.FloatingActionButton fab1 = findViewById(R.id.fab1);
+        com.github.clans.fab.FloatingActionButton fab2 = findViewById(R.id.fab2);
+        com.github.clans.fab.FloatingActionButton fab3 = findViewById(R.id.fab3);
+        com.github.clans.fab.FloatingActionButton fab4 = findViewById(R.id.fab4);
         menuRed.setClosedOnTouchOutside(true);
         menus.add(menuRed);
         menuRed.setOnMenuButtonClickListener(new View.OnClickListener() {
@@ -102,7 +107,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(10000)
                 .setFastestInterval(5000)
-                .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                        try {
+                            status.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+
+                        break;
+                }
+            }
+        });
         mLocationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
@@ -139,7 +170,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onLocationResult(LocationResult locationResult) {
                 super.onLocationResult(locationResult);
                 location = locationResult.getLastLocation();
-                fetchroutes(location, latLng);
+                if (location != previousLocation)
+                    fetchroutes(location, latLng);
+                else
+                    previousLocation = location;
 
             }
 
@@ -191,7 +225,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     markerOptions.position(latLng);
                     markerOptions.draggable(true);
                     markerOptions.title("Euclidean Distance " + computeDistanceBetween(new LatLng(lctn.getLatitude(), lctn.getLongitude()), latLng) + "m");
-                    markerOptions.snippet("Duration "+time+" Distance "+distance);
+                    markerOptions.snippet("Duration " + time + " Distance " + distance);
                     mMap.addMarker(markerOptions);
                 }
 
@@ -232,6 +266,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.fab1:
+                    removepolyline();
                     getDeviceLocation(points.get(0));
                     mFusedLocationProviderClient
                             .requestLocationUpdates(mLocationRequest,
@@ -239,6 +274,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     mRequestingLocationUpdates = true;
                     break;
                 case R.id.fab2:
+                    removepolyline();
                     getDeviceLocation(points.get(1));
                     mFusedLocationProviderClient
                             .requestLocationUpdates(mLocationRequest,
@@ -247,6 +283,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                     break;
                 case R.id.fab3:
+                    removepolyline();
                     getDeviceLocation(points.get(2));
                     mFusedLocationProviderClient
                             .requestLocationUpdates(mLocationRequest,
@@ -254,6 +291,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     mRequestingLocationUpdates = true;
                     break;
                 case R.id.fab4:
+                    removepolyline();
                     getDeviceLocation(points.get(3));
                     mFusedLocationProviderClient
                             .requestLocationUpdates(mLocationRequest,
@@ -263,5 +301,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     };
+
+    private void removepolyline() {
+        mMap.clear();
+        if (polyline1 != null)
+            polyline1.remove();
+        if (polylineFinal != null)
+            polylineFinal.remove();
+        if (mRequestingLocationUpdates) {
+            mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+            mRequestingLocationUpdates = false;
+        }
+    }
 
 }
